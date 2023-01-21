3cacb56c0c1a37fe0ec9ccee5ec259f6937356a7
aryanballani <aryanballani@gmail.com>
2023-01-21 14:58:17 -0800

    password hidden

--- diet-plan/index.js
+++ diet-plan/index.js
@@ -1,1 +1,8 @@
-console.log("Welcome to diet plan");
+function showPass() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
