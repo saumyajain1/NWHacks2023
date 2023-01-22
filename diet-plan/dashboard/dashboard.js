b5f0021cb392ec1f3c08b29c7a81612baeb81354
apram <apram235ahuja@gmail.com>
2023-01-21 21:06:07 -0800

    bmi + organization

--- diet-plan/dashboard/dashboard.js
+++ diet-plan/dashboard/dashboard.js
@@ -3,5 +3,11 @@ function findBMI() {
     var height = document.getElementById("height").value;
         // BMI = weight (kg) / height (m)^2
         var bmi = weight / (height * height/10000);
+        window.location.href = "dashboard.html#about";
         alert("Your BMI is: " + bmi);
+
     }
+
+function returnBMI(){
+    return bmi;
+}
